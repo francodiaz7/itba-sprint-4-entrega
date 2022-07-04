3817b3390884b2fe2e2da8bc9ac6832de9c3682c
Franco Diaz2 <francoagustindiaz7@gmail.com>
2022-07-03 23:38:00 -0300

    Cambiar open por with open

--- listCheq.py
+++ listCheq.py
@@ -3,53 +3,38 @@ import sys
 import datetime
 import time
 
+#Se define variables para filtrar el archivo según distintos argumentos
 
-#Falta guardar por csv (2c y 4), poder filtrar por fecha (2f), y mostrar error por pantala (3)
-#A considerar
-#sys.argv guarda todos los argumentos
-#sys.argv[0] regresa el nombre del script, por lo tanto el lenght de sys.argv se incrementa
-
-#Se definen funciones para filtrar las filas en el archivo csv
-
-#Al usar with open(...) cierra el archivo. Mientras que las otras funciones no (a confirmar).
-def buscarPorAlternativa():
+def buscarPorDniTipo():
     resultado = []
-    with open(archivo, 'r', newline='') as f:
-        reader = csv.reader(f)
-        #La linea de abajo saltea el header
+    with open(archivo, 'r', newline='') as file:
+        reader = csv.reader(file)
         next(reader, None)
         for row in reader:
             if dni==row[8] and tipoCheque==row[9]:
                 resultado.append(row)
         return resultado
 
-def buscarPorDniTipo():
-    resultado = []
-    csv_file = csv.reader(open(archivo, 'r'))
-    for row in csv_file:
-        if dni==row[8] and tipoCheque==row[9]:
-            resultado.append(row)
-    return resultado
-
 def buscarPorDniTipoEstado():
     resultado = []
-    csv_file = csv.reader(open(archivo, 'r'))
-    for row in csv_file:
-        if dni==row[8] and tipoCheque==row[9] and estadoCheque==row[10]:
-            resultado.append(row)
-    return resultado
-
-#Está incompleta, la fecha en el archivo csv está guardada en segundos.
-#abajo del todo hay un ejemplo de como usar datetime
+    with open(archivo, 'r', newline='') as file:
+        reader = csv.reader(file)
+        next(reader, None)
+        for row in reader:
+            if dni==row[8] and tipoCheque==row[9] and estadoCheque==row[10]:
+                resultado.append(row)
+        return resultado
 
 def buscarPorDniTipoEstadoFecha():
     resultado = []
-    csv_file = csv.reader(open(archivo, 'r'))
-    for row in csv_file:
-        ''' EJEMPLO CÓDIGO FECHAS USANDO TIMESTAMP
-         ts_now = time.time()
-         dt_now = datetime.fromtimestamp(ts_now)
-        '''
+    with open(archivo, 'r', newline='') as file:
+        reader = csv.reader(file)
+        next(reader, None)
+        for row in reader:
+            ''' EJEMPLO CÓDIGO FECHAS USANDO TIMESTAMP
+            ts_now = time.time()
+            dt_now = datetime.fromtimestamp(ts_now)
+            '''
 
         #creo que acá hay que buscar el estado del cheque para la fecha actual, no sé si me equivoco
         if dni==row[8] and tipoCheque==row[9] and estadoCheque==row[10]:
@@ -86,7 +71,6 @@ def chequeRepetido():
     if len(repetidos) == 0:
         print('ERROR')
 '''
-#Falta la opción para crear un archivo CSV
 
 def tipoDeSalida(resultado):
     if salida == "PANTALLA":
@@ -120,8 +104,6 @@ if __name__ == '__main__':
         tipoCheque = sys.argv[4]
 
     if len(sys.argv) == 5:
-        # mostrar = buscarPorAlternativa()
-        # tipoDeSalida(mostrar)
         resultado = buscarPorDniTipo()
         tipoDeSalida(resultado)
 
@@ -139,23 +121,3 @@ if __name__ == '__main__':
         rangoFecha = sys.argv[6]
         resultado = buscarPorDniTipoEstadoFecha()
         tipoDeSalida(resultado)
-
-    #Desde acá hasta abajo el código solo está con motivos de posibles opciones.
-    
-    """
-    #Itera sobre todos los argumentos
-    for arg in sys.argv:
-        if (arg == "-h" or arg == "-help" or arg == "--help"):
-            print("Ejecutar este codigo\
-                \n-h, -help, --help: Aparece este menú\
-                \ntime: Imprime la fecha actual (experimento)")
-
-        # imprime tiempo actual
-
-        if (arg == "time"):
-            fechaActual = datetime.datetime.now()
-            fechaActualFormato = datetime.datetime.strftime(fechaActual, '%d-%m-%Y %H:%M:%S')
-            # https://strftime.org
-            print(fechaActual)
-            print(fechaActualFormato)
-    """
